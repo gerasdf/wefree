739976ea4ee032d35f82edca20da1b02327caf95
gera <gera@satellogic.com>
2014-03-28 01:16:29 -0300

    starting with a complete rewrite

---
+++ wefree/model.py
@@ -0,0 +1,56 @@
+import time
+
+class AP(object):
+    CRYPTO_OPEN    = 0
+    CRYPTO_WEP     = 1
+    CRYPTO_WPA2    = 2
+    CRYPTO_UNKNOWN = 3
+
+    def __init__(self, ssid, bssid, crypto):
+        self.ssid   = ssid
+        self.bssid  = bssid
+        self.crypto = crypto
+    
+    def __eq__(self, ap2):
+        return self.bssid == ap2.bssid and self.ssid == ap2.ssid
+
+class VisibleSignal(object):
+    " There's an os specific abstraction for each Networking implementation "
+    def __init__(self, ap, strength = None):
+        self.ap = ap
+        self.strength = strength
+    
+    def connect(self, using_password = None):
+        """ Will try to connect to the signal using the suplied password.
+            If password is not supplied, all ap.possible_passwords will be tried
+            (and reported on)"""
+        raise Exception, "Subclass responsibility"
+    
+    def is_connected(self):
+        " Answers whether this signal is currently connected or not "
+        raise Exception, "Subclass responsibility"
+    
+class Configuration(object):
+    def __init__(self, ap, passwords):
+        self.ap = ap
+        self.passwords = passwords
+    
+class Report(object):
+    TYPE_SUCCESS  = 0
+    TYPE_FAILURE  = 1
+    TYPE_IMPORTED = 2
+    TYPE_UNKNOWN  = 3
+    
+    def __init__(self, ap, password = None, reporting = TYPE_UNKNOWN):
+        self.ap = ap
+        self.password = password
+        self.reporting = reporting
+        self.time_stamp = time.gmtime()
+
+class Location(object):
+    def __init__(self, lat = None, _long = None):
+        self.lat = lat
+        self.long = _long
+    
+    def is_unknown(self):
+        return self.lat is None or self.long is None
---
+++ wefree/osabstraction.py
@@ -0,0 +1,154 @@
+import dbus
+from dbus import DBusException
+from wefree import model
+
+try:
+    import NetworkManager
+    import uuid
+except (DBusException, ImportError):
+    pass
+
+class NetworkingBackend(object):
+    def __init__(self):
+        self.pending_signal = None
+        self.update_connected_state = lambda x:None
+
+    def device_state_changed(self, new_state, *args, **kwargs):
+        if self.pending_signal:
+            if self.CONNECTED_STATE == new_state:
+                if self.pending_signal.report_to_db:
+                    password = self.pending_signal.passwords()[0]
+#                    PM.report_success(self.pending_signal.ssid, self.pending_signal.bssid, password, success = True)
+                self.pending_signal.report_to_db = False
+                self.pending_signal = None
+            elif self.FAILED_STATE == new_state:
+                self.pending_signal = None
+                self.report_to_db = False
+        self.update_connected_state(new_state == self.CONNECTED_STATE)
+
+    def register_events(self, update_connected_state, refresh_menu_items):
+        self.update_connected_state = update_connected_state
+    
+    def connect(self, signal):
+        self.pending_signal = signal
+        signal.connect()
+
+    def is_connected(self, ap):
+        return ap in self.currently_connected_configurations()
+    
+    def currently_connected_configurations(self):
+        " Answers a list of the configurations that're currently active and connected "
+        raise Exception, "Subclass responsibility"
+
+# NetworkManager implementation
+
+class NetworkManagerVisibleSignal(model.VisibleSignal):
+    def __init__(self, ap, strength=None, device = None):
+        model.VisibleSignal.__init__(self, ap, strength=strength)
+        self.device = device
+    
+    def connect(self, using_password=None):
+        raise Exception, 'Not implemented yet'
+    
+class NetworkManagerBackend(NetworkingBackend):
+    @staticmethod
+    def is_installed():
+        try:
+            NetworkManager.NetworkManager.state()
+            return True
+        except DBusException:
+            return False
+
+    def __init__(self):
+        NetworkingBackend.__init__(self)
+        self.CONNECTED_STATE = NetworkManager.NM_DEVICE_STATE_ACTIVATED
+        self.FAILED_STATE = NetworkManager.NM_DEVICE_STATE_FAILED
+
+    def register_events(self, update_connected_state, refresh_menu_items):
+        NetworkingBackend.register_events(self, update_connected_state, refresh_menu_items)
+        for device in NetworkManager.NetworkManager.GetDevices():
+            device.connect_to_signal("AccessPointAdded", refresh_menu_items)
+            device.connect_to_signal("AccessPointRemoved", refresh_menu_items)
+            device.connect_to_signal("StateChanged", self.device_state_changed, sender_keyword=device)
+
+    def wifi_devices(self):
+        all_devices = NetworkManager.NetworkManager.GetDevices()
+        return [device for device in all_devices if device.DeviceType == NetworkManager.NM_DEVICE_TYPE_WIFI]
+
+    def AP(self, network_manager_AP):
+        if (network_manager_AP.WpaFlags | network_manager_AP.RsnFlags):
+            # ToDo: use constants like NM_802_11_AP_SEC_KEY_MGMT_PSK:
+            crypto = model.AP.CRYPTO_WPA2
+        else:
+            crypto = model.AP.CRYPTO_OPEN
+
+        ap = model.AP(network_manager_AP.Ssid, network_manager_AP.HwAddress, crypto)
+        return ap
+
+    def list_visible_signals(self):
+        """Get the wifi currently visible signals."""
+        answer = []
+        for device in self.wifi_devices():
+            nm_aps = device.SpecificDevice().GetAccessPoints()
+            for nm_ap in nm_aps:
+                ap = self.AP(nm_ap)
+                strength = ord(nm_ap.Strength) / 100.0
+                signal = NetworkManagerVisibleSignal(ap, strength, device)
+                answer.append(signal)
+        return answer
+
+
+# Wicd implementation
+
+    def currently_connected_configurations(self):
+        answer = []
+        for device in self.wifi_devices():
+            nm_ap = device.SpecificDevice().ActiveAccessPoint
+            answer.append(self.AP(nm_ap))
+        return answer
+
+class WicdVisibleSignal(model.VisibleSignal):
+    def connect(self, using_password=None):
+        raise Exception, 'Not implemented yet'
+    
+    def is_connected(self):
+        raise Exception, 'Not implemented yet'
+    
+class WicdBackend(NetworkingBackend):
+    @staticmethod
+    def is_installed():
+        try:
+            dbus.SystemBus().get_object('org.wicd.daemon','/org/wicd/daemon/wireless')
+            return True
+        except DBusException:
+            return False
+
+    def __init__(self):
+        NetworkingBackend.__init__(self)
+        self.CONNECTED_STATE = 2
+        self.FAILED_STATE = 0
+        self.bus = dbus.SystemBus()
+        self.wireless = dbus.Interface(
+            self.bus.get_object('org.wicd.daemon',
+                                '/org/wicd/daemon/wireless'),
+            'org.wicd.daemon.wireless'
+        )
+
+    def register_events(self, update_connected_state, refresh_menu_items):
+        NetworkingBackend.register_events(self, update_connected_state, refresh_menu_items)
+        self.bus.add_signal_receiver(self.device_state_changed,
+                                     'StatusChanged','org.wicd.daemon',
+                                     'org.wicd.daemon', '/org/wicd/daemon')
+        self.bus.add_signal_receiver(refresh_menu_items,
+                                     'SendEndScanSignal',
+                                     'org.wicd.daemon.wireless',
+                                     'org.wicd.daemon',
+                                     '/org/wicd/daemon/wireless')
+
+
+if NetworkManagerBackend.is_installed():
+    Networking = NetworkManagerBackend()
+elif WicdBackend.is_installed():
+    Networking = WicdBackend()
+
+print
